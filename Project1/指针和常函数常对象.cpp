076815202ecb06f4bedc3876704d5c0a7dd54870
zhangrui07 <zhangrui07@shengqugames.com>
2022-03-19 16:37:07 +0800

    231321

--- Project1/指针和常函数常对象.cpp
+++ Project1/指针和常函数常对象.cpp
@@ -1,53 +1,53 @@
-#include<iostream>;
-using namespace std;
-
-const int MAX = 10;
-struct person {
-	string name;
-	int number;
-};
-struct book
-{
-	struct person personArr[MAX];
-	int count;
-};
-
-void test(int &a,int &b) {
-	
-	int temp = b;
-	b = a;
-	a = temp;
-}
-class Person {
-public:
-	static void Test() {
-		age = 0;
-		cout << "返回的e地址" << age <<endl;
-	};
-	Person* Test2() {
-		//this->age = 0;
-		return this;
-	};
-	static int age;
-};
-
-int Person::age = 0;
-int main() {
-	Person* p = NULL;
-	p->age;
-	p->Test();
-	Person p2;
-	Person::Test();
-	p2.Test();;
-	/*Person p;
-	Person* q = &p;
-	Person& e = *q;
-	cout << "返回的e地址" << &e << endl;
-	cout << "返回的e地址" << &p << endl;*/
-	/*cout << "返回的q地址" << q << endl;
-	Person* x = p.Test().Test2();
-	cout << "返回的p地址" << x <<endl;*/
-	//123124
-	system("pause");
-	return 0;
-}
+//#include<iostream>;
+//using namespace std;
+//
+//const int MAX = 10;
+//struct person {
+//	string name;
+//	int number;
+//};
+//struct book
+//{
+//	struct person personArr[MAX];
+//	int count;
+//};
+//
+//void test(int &a,int &b) {
+//	
+//	int temp = b;
+//	b = a;
+//	a = temp;
+//}
+//class Person {
+//public:
+//	static void Test() {
+//		age = 0;
+//		cout << "返回的e地址" << age <<endl;
+//	};
+//	Person* Test2() {
+//		//this->age = 0;
+//		return this;
+//	};
+//	static int age;
+//};
+//
+//int Person::age = 0;
+//int main() {
+//	Person* p = NULL;
+//	p->age;
+//	p->Test();
+//	Person p2;
+//	Person::Test();
+//	p2.Test();;
+//	/*Person p;
+//	Person* q = &p;
+//	Person& e = *q;
+//	cout << "返回的e地址" << &e << endl;
+//	cout << "返回的e地址" << &p << endl;*/
+//	/*cout << "返回的q地址" << q << endl;
+//	Person* x = p.Test().Test2();
+//	cout << "返回的p地址" << x <<endl;*/
+//	//123124
+//	system("pause");
+//	return 0;
+//}
